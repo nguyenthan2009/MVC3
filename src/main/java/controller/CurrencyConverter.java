4424787fca95e9bbac23f278748ff13bbaee4778
unknown <sauvole75@gmail.com>
2021-09-10 16:17:29 +0700

    'add'

---
+++ src/main/java/controller/CurrencyConverter.java
@@ -0,0 +1,26 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CurrencyConverter {
+    @GetMapping("/than")
+    public String form(){
+        return "currencyconverter";
+
+    }
+    @PostMapping("/converter")
+    public String converter(@RequestParam double rate, @RequestParam double usd, Model model) {
+        double vnd = rate * usd;
+        model.addAttribute("rate", rate);
+        model.addAttribute("usd", usd);
+        model.addAttribute("vnd", vnd);
+        return "value";
+    }
+
+
+}
